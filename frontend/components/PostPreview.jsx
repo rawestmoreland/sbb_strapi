29f23f2cb519e9668fa3ff27a6a7f94a59d42176
Richard Westmoreland <kegtrack@gmail.com>
2022-01-01 09:42:45 -0500

    articles layout

--- frontend/components/PostPreview.jsx
+++ frontend/components/PostPreview.jsx
@@ -1,19 +1,22 @@
 import Image from './Image'
+import Link from 'next/link'
 import { format } from 'date-fns'
 
 const PostPreview = ({ post }) => {
 	const { image } = post
 	return (
-		<div className='flex flex-col md:flex-row w-full gap-x-8'>
-			<div className='w-full'>
-				<Image media={image} />
+		<Link as={`post/${post.slug}`} href='/post/[id]'>
+			<div className='grid grid-cols-3 w-full gap-x-8 cursor-pointer'>
+				<div className='col-span-1 w-full h-full'>
+					<Image media={image} />
+				</div>
+				<div className='flex flex-col col-span-2'>
+					<h2 className='text-2xl font-bold mb-2'>{post.title}</h2>
+					<p className='mb-2'>{post.description}</p>
+					<p>{format(new Date(post.published), 'LLLL do, yyyy')}</p>
+				</div>
 			</div>
-			<div className='flex flex-col justify-between'>
-				<h2 className='text-2xl font-bold'>{post.title}</h2>
-				<p>{post.description}</p>
-				<p>{format(new Date(post.published), 'LLLL do, yyyy')}</p>
-			</div>
-		</div>
+		</Link>
 	)
 }
 
---
+++ frontend/pages/post/[slug].js
@@ -0,0 +1,56 @@
+import ReactMarkdown from 'react-markdown'
+import { format } from 'date-fns'
+import { fetchAPI, getStrapiMedia } from '../../utils/api-helpers'
+import Layout from '../../components/Layout'
+import Image from '../../components/Image'
+import Seo from '../../components/Seo'
+
+const Post = ({ post }) => {
+	const imageUrl = getStrapiMedia(post.image.url)
+
+	const seo = {
+		metaTitle: post.title,
+		metaDescription: post.description,
+		shareImage: post.image,
+		article: true,
+	}
+
+	return (
+		<Layout>
+			<Seo seo={seo} />
+			<div className='postContent'>
+				<Image media={post.image} />
+				<h1>{post.title}</h1>
+				<div className='my-4 text-gray-600'>
+					<em>{`Author: ${post.author.name}`}</em>
+				</div>
+				<hr className='mb-6' />
+				<ReactMarkdown children={post.content} />
+			</div>
+		</Layout>
+	)
+}
+
+export async function getStaticPaths() {
+	const posts = await fetchAPI('/posts')
+
+	return {
+		paths: posts.map((post) => ({
+			params: {
+				slug: post.slug,
+			},
+		})),
+		fallback: false,
+	}
+}
+
+export async function getStaticProps({ params }) {
+	const posts = await fetchAPI(`/posts?slug=${params.slug}`)
+
+	return {
+		props: { post: posts[0] },
+		revalidate: 1,
+	}
+}
+
+export default Post
