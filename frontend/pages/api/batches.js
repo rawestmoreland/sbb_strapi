77e3a1bc26bc24e7d0b8a26c6f08c7941ee0cd29
Richard Westmoreland <kegtrack@gmail.com>
2022-01-07 16:54:27 -0600

    disable redis cache

--- frontend/pages/api/batches.js
+++ frontend/pages/api/batches.js
@@ -4,17 +4,17 @@ const Redis = require('ioredis')
 const { BASE_API_URL } = require('./utils/constants')
 
 export default async function handler(req, res) {
-	const redis = new Redis(process.env.REDIS_URL)
+	// const redis = new Redis(process.env.REDIS_URL)
 	const authString = `${process.env.BREWFATHER_USER_ID}:${process.env.BREWFATHER_API_KEY}`
 	const encodedAuth = Buffer.from(authString).toString('base64')
 	const { page, offset, limit } = req.body
 	try {
-		const cachedData = await redis.get(`batches${page}`)
+		// const cachedData = await redis.get(`batches${page}`)
 
-		if (cachedData) {
-			console.log('Returning cached data')
-			return res.status(200).json(cachedData)
-		}
+		// if (cachedData) {
+		// 	console.log('Returning cached data')
+		// 	return res.status(200).json(cachedData)
+		// }
 
 		const response = await fetch(
 			`${BASE_API_URL}/batches?offset=${offset}&limit=${limit}`,
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
 		const batches = await response.json()
 
-		redis.set(`batches${page}`, JSON.stringify(batches), 'EX', 45)
+		// redis.set(`batches${page}`, JSON.stringify(batches), 'EX', 45)
 
 		console.log('Returning fresh data')
 
