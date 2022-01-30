65f1f5ae31c96d092da6f48a9d7f736e883cc3ce
Richard Westmoreland <kegtrack@gmail.com>
2022-01-30 09:21:23 -0600

    fix up the fetching params

--- frontend/components/InProgressBatch.jsx
+++ frontend/components/InProgressBatch.jsx
@@ -1,10 +1,9 @@
-import { format } from 'date-fns'
+import { format } from 'date-fns-tz'
 
 const InProgressBatch = ({ batchData }) => {
-	console.log(batchData)
 	const { batchName, batchStatus } = batchData
-	const { sg, timepoint } = batchData.tilt
-	const formattedTime = format(new Date(timepoint), 'Pp')
+	const { sg, time } = batchData.tilt
+	const formattedTime = format(new Date(time), 'Pp')
 	return (
 		<div>
 			<h3 className='mb-4 font-medium text-xl'>In Progress</h3>
@@ -13,7 +12,9 @@ const InProgressBatch = ({ batchData }) => {
 			<h4 className='text-lg'>Status:</h4>
 			<div className='mb-2 text-sm'>{batchStatus}</div>
 			<h4 className='text-lg'>Last Reading:</h4>
-			<div className='text-sm'>{`${sg} at ${formattedTime}`}</div>
+			<div className='text-sm'>{`${sg.toFixed(
+				3
+			)} at ${formattedTime}`}</div>
 		</div>
 	)
 }
--- frontend/pages/api/fermenting.js
+++ frontend/pages/api/fermenting.js
@@ -4,13 +4,7 @@ const { fetchFromBrewfather } = require('./utils/brewfather-helpers')
 const redis = new Redis(process.env.REDIS_URL)
 
 export default async function handler(req, res) {
-	const authString = `${process.env.BREWFATHER_USER_ID}:${process.env.BREWFATHER_API_KEY}`
-	const encodedAuth = Buffer.from(authString).toString('base64')
-
-	const fermenting = await fetchFromBrewfather(
-		encodedAuth,
-		`/batches?status=Fermenting`
-	)
+	const fermenting = await fetchFromBrewfather(`/batches?status=Fermenting`)
 
 	return res.status(200).json({ data: fermenting })
 }
