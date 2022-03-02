d0fd497faf854cc56d7e94f40acb6ca3e7c9940d
Richard Westmoreland <kegtrack@gmail.com>
2022-03-01 20:27:18 -0600

    export

--- backend/app/config/functions/utils/helpers.js
+++ backend/app/config/functions/utils/helpers.js
@@ -1,5 +1,5 @@
-export function getBaseURL() {
+export const getBaseURL = () => {
   return `${process.env.NEXT_SBB_PUBLIC_URL || "http://localhost:3000"}${path}`;
-}
+};
 
 module.exports = { getBaseURL };
