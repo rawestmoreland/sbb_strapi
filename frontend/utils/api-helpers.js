95373de6d6afe4c7559f06f541328aa98a992787
Richard Westmoreland <kegtrack@gmail.com>
2022-03-01 20:54:45 -0600

    deploy preview things

--- frontend/utils/api-helpers.js
+++ frontend/utils/api-helpers.js
@@ -43,6 +43,8 @@ export async function fetchLastReading() {
 	const fetchUrl =
 		process.env.NODE_ENV === 'production'
 			? 'https://strapi-sbb.netlify.app/api/lasttiltreading'
+			: process.env.DEPLOY_PREVIEW === 'deploy-preview'
+			? `${process.env.DEPLOY_PRIME_URL}`
 			: 'http://localhost:3000/api/lasttiltreading'
 
 	const res = await fetch(fetchUrl, {
