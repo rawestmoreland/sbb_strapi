a7969d2cdd3bf4d31745c61468345e7da3941302
Richard Westmoreland <kegtrack@gmail.com>
2022-03-03 21:15:23 -0600

    more logs

--- frontend/pages/api/subscribe.js
+++ frontend/pages/api/subscribe.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
 		}
 
 		try {
+			console.log('google recaptcha verify')
 			// Ping the google recaptcha verify API to verify the captcha code you received
 			const response = await fetch(
 				`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
