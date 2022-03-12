f43a6fbfe7baabac56d4af8d6083c6c236ded09c
Richard Westmoreland <kegtrack@gmail.com>
2022-03-11 22:36:37 -0600

    import dotenv

--- backend/app/api/post/models/post.js
+++ backend/app/api/post/models/post.js
@@ -1,9 +1,12 @@
 "use strict";
 
+require("dotenv").config;
+
 module.exports = {
   lifecycles: {
     async beforeUpdate(params, data) {
       if (data.published_at != null) {
+        const strapiURL = process.env.NEXT_PUBLIC_STRAPI_URL;
         const { id } = params;
         const previousData = await strapi.query("post").findOne({ id });
         const previousPublishedAt = previousData.published_at;
@@ -12,7 +15,10 @@ module.exports = {
           .query("subscribers")
           .find({ activated: true });
         console.log("CONTEXT", process.env.NEXT_PUBLIC_STRAPI_URL);
-        if (currentPublished_at != previousPublishedAt) {
+        if (
+          currentPublished_at != previousPublishedAt &&
+          strapiURL?.contains("strapi-sbb-prod")
+        ) {
           subList.forEach(async (sub) => {
             await strapi.services.post.sendPost(
               sub.email,
