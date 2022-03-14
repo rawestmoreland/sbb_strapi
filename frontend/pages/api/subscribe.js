d0362ea5aeca96e42f6e38be3ac3eeb26e181530
Richard Westmoreland <kegtrack@gmail.com>
2022-03-14 07:49:22 -0500

    revert

--- frontend/pages/api/subscribe.js
+++ frontend/pages/api/subscribe.js
@@ -15,20 +15,20 @@ export default async function handler(req, res) {
 			})
 		}
 
-		// try {
-		// Ping the google recaptcha verify API to verify the captcha code you received
-		const response = await fetch(
-			`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
-			{
-				headers: {
-					'Content-Type':
-						'application/x-www-form-urlencoded; charset=utf-8',
-				},
-				method: 'POST',
-			}
-		)
-		const captchaValidation = await response.json()
-		/**
+		try {
+			// Ping the google recaptcha verify API to verify the captcha code you received
+			const response = await fetch(
+				`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
+				{
+					headers: {
+						'Content-Type':
+							'application/x-www-form-urlencoded; charset=utf-8',
+					},
+					method: 'POST',
+				}
+			)
+			const captchaValidation = await response.json()
+			/**
        * The structure of response from the verify API is
        * {
        *  "success": true|false,
@@ -37,24 +37,24 @@ export default async function handler(req, res) {
        *  "error-codes": [...]        // optional
         }
        */
-		if (captchaValidation.success) {
-			const response = await strapiPostSubscribe(
-				'/subscribers',
-				{
-					method: 'POST',
-					body: JSON.stringify({ email }),
-				},
-				email
-			)
+			if (captchaValidation.success) {
+				const response = await strapiPostSubscribe(
+					'/subscribers',
+					{
+						method: 'POST',
+						body: JSON.stringify({ email }),
+					},
+					email
+				)
 
-			return res.status(200).send(JSON.stringify(response))
+				return res.status(200).send(JSON.stringify(response))
+			}
+			return res.status(422).json({
+				message: 'Unproccesable request, Invalid captcha code',
+			})
+		} catch (error) {
+			return res.status(422).json({ message: error.message })
 		}
-		// return res.status(422).json({
-		// 	message: 'Unproccesable request, Invalid captcha code',
-		// })
-		// } catch (error) {
-		// 	return res.status(422).json({ message: error.message })
-		// }
 	}
 	// Return 404 if someone pings the API with a method other than
 	// POST
