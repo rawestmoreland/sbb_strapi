c939f6f03e3160cf7a1f7d66dadeea0ef9992966
Richard Westmoreland <kegtrack@gmail.com>
2022-03-15 11:23:08 -0500

    use netlify form

--- frontend/components/SubscribeForm.js
+++ frontend/components/SubscribeForm.js
@@ -3,71 +3,38 @@ import ReCAPTCHA from 'react-google-recaptcha'
 
 const SubscribeForm = () => {
 	const [email, setEmail] = useState('')
-	const recaptchaRef = createRef()
-
-	const handleChange = (event) => {
-		setEmail(event.target.value)
+	const handleChange = (e) => {
+		setEmail(e.target.value)
 	}
-
-	const registerUser = (event) => {
-		event.preventDefault()
-		recaptchaRef.current.execute()
-	}
-
-	const onReCAPTCHAChange = async (captchaCode) => {
-		// If the reCAPTCHA code is null or undefined indicating that
-		// the reCAPTCHA was expired then return early
-		if (!captchaCode) {
-			return
-		}
-
-		const fetchUrl =
-			process.env.NEXT_PUBLIC_SBB_URL || 'http://localhost:3000'
-
-		try {
-			const response = await fetch(`${fetchUrl}/api/subscribe`, {
-				method: 'POST',
-				body: JSON.stringify({ email, captcha: captchaCode }),
-				headers: {
-					'Content-type': 'application/json; charset=UTF-8',
-					'Access-Control-Allow-Origin': '*',
-				},
-			})
-			if (response.ok) {
-				const { message } = await response.json()
-				// If the response is ok, show an alert.
-				alert(message)
-			} else {
-				const error = await response.json()
-				// Else throw an error with the message returned
-				// from the API
-				alert(error.message)
+	const handleSubmit = (form) => {
+		const data = new FormData('subscribe', 'newsletter')
+		fetch('/', {
+			method: 'POST',
+			body: data,
+		})
+			.then(() => {
+				alert('Thanks! Check your email to confirm your email address.')
 				setEmail('')
-			}
-		} catch (error) {
-			alert(
-				error?.message ??
-					'Something went wrong. Unable to register right now.'
-			)
-		} finally {
-			recaptchaRef.current.reset()
-			setEmail('')
-		}
+			})
+			.catch((error) => alert(error))
 	}
 
 	return (
 		<form
 			className='flex flex-col items-start gap-y-2'
-			onSubmit={registerUser}
+			name='subscribe'
+			method='POST'
+			data-netlify='true'
+			netlify-honeypot='got-ya'
+			onSubmit={handleSubmit}
 		>
-			<ReCAPTCHA
-				ref={recaptchaRef}
-				size='invisible'
-				sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-				onChange={onReCAPTCHAChange}
-			/>
 			<p className='text-sm'>
 				Sign up to be notified when we publish new content!
+			</p>
+			<p className='hidden'>
+				<label>
+					Don't fill this in if you're human. <input type='got-ya' />
+				</label>
 			</p>
 			<input
 				className='border p-2 w-full text-xs'
