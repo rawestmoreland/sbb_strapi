8fbde6fae9a526c53fa9aea5b9999f5c833b3839
Richard Westmoreland <kegtrack@gmail.com>
2022-03-16 19:12:35 -0500

    forms

---
+++ frontend/pages/contact/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SubscribeForm from '../../components/SubscribeForm'
+
+export default function ContactPage() {
+	return (
+		<div>
+			<SubscribeForm />
+		</div>
+	)
+}
