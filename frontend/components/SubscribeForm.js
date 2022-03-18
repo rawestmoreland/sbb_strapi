99eda4d82bbc368e344dfa69d3fff8b8db99fd93
Richard Westmoreland <kegtrack@gmail.com>
2022-03-18 06:07:10 -0500

    new form

--- frontend/components/SubscribeForm.js
+++ frontend/components/SubscribeForm.js
@@ -1,78 +1,77 @@
-import { useForm } from 'react-hook-form'
+import React, { useState } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 const SubscribeForm = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm()
+	const recaptchaRef = React.createRef()
 
-	const encode = (data) => {
-		return Object.keys(data)
-			.map(
-				(key) =>
-					encodeURIComponent(key) +
-					'=' +
-					encodeURIComponent(data[key])
-			)
-			.join('&')
+	const [email, setEmail] = useState('')
+
+	const handlePost = (event) => {
+		event.preventDefault()
+		// Execute the reCAPTCHA when the form is submitted
+		recaptchaRef.current.execute()
 	}
 
-	const handlePost = (formData, e) => {
-		fetch('/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: encode({
-				'form-name': 'subscribe',
-				...formData,
-			}),
-		})
-			.then(() => {
-				console.log('data sent to netlify')
-				reset()
+	const onChange = (e) => {
+		setEmail(e.target.value)
+	}
+
+	const onReCAPTCHAChange = async (captchaCode) => {
+		// If the reCAPTCHA code is null or undefined indicating that
+		// the reCAPTCHA was expired then return early
+		if (!captchaCode) {
+			return
+		}
+		try {
+			const response = await fetch('/api/subscribe', {
+				method: 'POST',
+				body: JSON.stringify({ email, captcha: captchaCode }),
+				headers: {
+					'Content-Type': 'application/json',
+				},
 			})
-			.catch((e) =>
-				alert('There was a problem with your submission. ' + e)
-			)
-		e.preventDefault()
+			if (response.ok) {
+				// If the response is ok than show the success alert
+				alert('Email registered successfully')
+			} else {
+				// Else throw an error with the message returned
+				// from the API
+				const error = await response.json()
+				throw new Error(error.message)
+			}
+		} catch (error) {
+			alert(error?.message || 'Something went wrong')
+		} finally {
+			// Reset the reCAPTCHA when the request has failed or succeeeded
+			// so that it can be executed again if user submits another email.
+			recaptchaRef.current.reset()
+			setEmail('')
+		}
 	}
 
 	return (
-		<div>
-			<form
-				onSubmit={handleSubmit(handlePost)}
-				className='flex flex-col items-start gap-y-2'
-				data-netlify='true'
-				netlify-honeypot='got-ya'
-				name='subscribe'
-				method='POST'
-			>
-				<p className='text-sm'>
-					Sign up to be notified when we publish new content!
-				</p>
-				<p className='hidden'>
-					<label>
-						Don't fill this in if you're human.
-						<input name='got-ya' {...register} />
-					</label>
-				</p>
-				<input type='hidden' name='form-name' value='subscribe' />
-				<input
-					className='border p-2 w-full text-xs'
-					aria-label='email'
-					placeholder='Email address'
-					name='email'
-					{...register('email')}
-				/>
-				<button
-					className='bg-black text-white p-2 rounded'
-					type='submit'
-				>
-					Subscribe
-				</button>
-			</form>
-		</div>
+		<form onSubmit={handlePost}>
+			<ReCAPTCHA
+				ref={recaptchaRef}
+				size='invisible'
+				sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+				onChange={onReCAPTCHAChange}
+			/>
+			<p className='text-sm'>
+				Sign up to be notified when we publish new content!
+			</p>
+			<input
+				className='h-8 my-2 p-2 border border-black rounded'
+				type='text'
+				name='email'
+				value={email}
+				onChange={onChange}
+				required
+			/>
+			<button className='bg-black text-white p-2 rounded' type='submit'>
+				Subscribe
+			</button>
+		</form>
 	)
 }
 
