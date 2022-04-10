5cb68dfde0a6a80095cf5795d946707e416b9e17
Richard Westmoreland <kegtrack@gmail.com>
2022-04-10 09:31:29 -0500

    prod db file

---
+++ backend/app/config/env/production/database.js
@@ -0,0 +1,30 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(
+    env("DATABASE_URL") ||
+      env("HEROKU_POSTGRESQL_GOLD_URL") ||
+      env("HEROKU_POSTGRESQL_COBALT_URL")
+  );
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false },
+        },
+        options: {
+          ssl: false,
+        },
+      },
+    },
+  };
+};
