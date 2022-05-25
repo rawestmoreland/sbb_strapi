07c49f0af9020cc6b361d5b60dc8c32039ef4f8c
Richard Westmoreland <kegtrack@gmail.com>
2022-05-24 22:34:12 -0500

    staging

--- frontend/plugins/netlify-plugin-env-var-by-context/index.js
+++ frontend/plugins/netlify-plugin-env-var-by-context/index.js
@@ -1,16 +1,16 @@
 module.exports = {
 	onPreBuild: async ({ inputs }) => {
-		console.log(process.env.CONTEXT)
-		console.log(process.env.BRANCH)
-
-		const context = process.env.CONTEXT.toUpperCase().replace(/-/g, '_')
-		Object.keys(process.env).forEach((key) => {
-			const envVar = `${context}_${key}`
-			const val = process.env[envVar]
-			if (process.env[envVar]) {
-				console.log(`Exporting ${key}=${val}`)
-				process.env[key] = val
-			}
-		})
+		if (process.env.BRANCH !== 'production') {
+			const context = process.env.BRANCH.toUpperCase().replace(/-/g, '_')
+			Object.keys(process.env).forEach((key) => {
+				const envVar = `${context}_${key}`
+				console.log({ envVar })
+				const val = process.env[envVar]
+				if (process.env[envVar]) {
+					console.log(`Exporting ${key}=${val}`)
+					process.env[key] = val
+				}
+			})
+		}
 	},
 }
