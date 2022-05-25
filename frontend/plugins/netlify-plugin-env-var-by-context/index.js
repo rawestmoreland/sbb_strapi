030514176b08274d7d800dca6edda53033f99d97
Richard Westmoreland <kegtrack@gmail.com>
2022-05-25 13:52:28 -0500

    plugin

--- frontend/plugins/netlify-plugin-env-var-by-context/index.js
+++ frontend/plugins/netlify-plugin-env-var-by-context/index.js
@@ -4,7 +4,6 @@ module.exports = {
 			const context = process.env.BRANCH.toUpperCase().replace(/-/g, '_')
 			Object.keys(process.env).forEach((key) => {
 				const envVar = `${context}_${key}`
-				console.log({ envVar })
 				const val = process.env[envVar]
 				if (process.env[envVar]) {
 					console.log(`Exporting ${key}=${val}`)
