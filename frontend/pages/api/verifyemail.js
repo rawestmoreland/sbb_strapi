5a43a69ea4d261f5202afffc4f300b720893eeb0
Richard Westmoreland <kegtrack@gmail.com>
2022-05-24 21:29:59 -0500

    changes

--- frontend/pages/api/verifyemail.js
+++ frontend/pages/api/verifyemail.js
@@ -4,6 +4,5 @@ import { fetchAPIAuth } from '../../utils/api-helpers'
 export default async function handler(req, res) {
 	const { token } = req.body
 	const updatedUser = await fetchAPIAuth(`/api/confirm-subscription/${token}`)
-	console.log(updatedUser)
 	return res.status(200).json({ data: updatedUser })
 }
