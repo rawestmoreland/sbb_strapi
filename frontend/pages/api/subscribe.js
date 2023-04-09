c858e4ea209b52ddc69deba2dfb546bf728845f4
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2023-04-09 16:06:35 -0500

    upgrade to node 18

--- frontend/pages/api/subscribe.js
+++ frontend/pages/api/subscribe.js
@@ -1,34 +1,30 @@
-import fetch from 'node-fetch'
-
 export default async function handler(req, res) {
-	const { body, method } = req
+  const { body, method } = req;
 
-	// Extract the email and captcha code from the request body
-	const { email, captcha } = body
+  // Extract the email and captcha code from the request body
+  const { email, captcha } = body;
 
-	if (method === 'POST') {
-		// If email or captcha are missing return an error
-		if (!email || !captcha) {
-			return res.status(422).json({
-				message:
-					'Unprocessable request, please provide the required fields',
-			})
-		}
+  if (method === 'POST') {
+    // If email or captcha are missing return an error
+    if (!email || !captcha) {
+      return res.status(422).json({
+        message: 'Unprocessable request, please provide the required fields',
+      });
+    }
 
-		try {
-			// Ping the google recaptcha verify API to verify the captcha code you received
-			const response = await fetch(
-				`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
-				{
-					headers: {
-						'Content-Type':
-							'application/x-www-form-urlencoded; charset=utf-8',
-					},
-					method: 'POST',
-				}
-			)
-			const captchaValidation = await response.json()
-			/**
+    try {
+      // Ping the google recaptcha verify API to verify the captcha code you received
+      const response = await fetch(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+          },
+          method: 'POST',
+        }
+      );
+      const captchaValidation = await response.json();
+      /**
        * The structure of response from the verify API is
        * {
        *  "success": true|false,
@@ -37,46 +33,38 @@ export default async function handler(req, res) {
        *  "error-codes": [...]        // optional
         }
        */
-			if (captchaValidation.success) {
-				const strapiURL =
-					process.env.NEXT_PUBLIC_STRAPI_URL ||
-					'http://localhost:1338'
+      if (captchaValidation.success) {
+        const strapiURL =
+          process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1338';
 
-				const strapiResponse = await fetch(
-					`${strapiURL}/api/subscribers`,
-					{
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify({ data: { email } }),
-					}
-				)
+        const strapiResponse = await fetch(`${strapiURL}/api/subscribers`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ data: { email } }),
+        });
 
-				const { data, error } = await strapiResponse.json()
+        const { data, error } = await strapiResponse.json();
 
-				if (strapiResponse.ok) {
-					return res.status(200).json({ data: data.attributes })
-				}
+        if (strapiResponse.ok) {
+          return res.status(200).json({ data: data.attributes });
+        }
 
-				if (error?.status && error?.message) {
-					return res
-						.status(error.status)
-						.json({ message: error.message })
-				}
-				return res
-					.status(500)
-					.json({ message: 'Internal server error' })
-			}
+        if (error?.status && error?.message) {
+          return res.status(error.status).json({ message: error.message });
+        }
+        return res.status(500).json({ message: 'Internal server error' });
+      }
 
-			return res.status(422).json({
-				message: 'Unproccesable request, Invalid captcha code',
-			})
-		} catch (error) {
-			return res.status(422).json({ message: 'Something went wrong' })
-		}
-	}
-	// Return 404 if someone pings the API with a method other than
-	// POST
-	return res.status(404).send('Not found')
+      return res.status(422).json({
+        message: 'Unproccesable request, Invalid captcha code',
+      });
+    } catch (error) {
+      return res.status(422).json({ message: 'Something went wrong' });
+    }
+  }
+  // Return 404 if someone pings the API with a method other than
+  // POST
+  return res.status(404).send('Not found');
 }
