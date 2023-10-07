85615e6597e8995d89223dcfa0eb25b235e5bcd9
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2023-10-07 11:53:23 +0100

    chore: change out the ad

--- frontend/components/SideBar.jsx
+++ frontend/components/SideBar.jsx
@@ -1,16 +1,34 @@
-import Image from "next/legacy/image"
-import InProgressBatch from './InProgressBatch'
-import SubscribeForm from './SubscribeForm'
+import Image from 'next/legacy/image';
+import InProgressBatch from './InProgressBatch';
+import SubscribeForm from './SubscribeForm';
 
 const SideBar = ({ batchData }) => {
-	return (
-		<div className='flex flex-col gap-y-8 md:col-span-1'>
-			<section>
-				<h3 className='mb-2 font-medium text-xl'>Subscribe</h3>
-				<SubscribeForm />
-			</section>
-			<section>
-				<h3 className='mb-4 font-medium text-xl'>Try Brewfather</h3>
+  return (
+    <div className='flex flex-col gap-y-8 md:col-span-1'>
+      <section>
+        <h3 className='mb-2 text-xl font-medium'>Subscribe</h3>
+        <SubscribeForm />
+      </section>
+      <section>
+        <h3 className='mb-4 text-xl font-medium'>Try Keg Track!</h3>
+        <p className='mb-4 text-xs'>
+          Keg volume monitoring for iOS and Android
+        </p>
+        <a
+          href='https://kegtrackapp.com'
+          target='_blank'
+          rel='noopener noreferer'
+          className='relative w-full'
+        >
+          <Image
+            style={{ borderRadius: '10%' }}
+            src='/kegtrack_icon.png'
+            alt='kegtrack icon'
+            width='150'
+            height='150'
+          />
+        </a>
+        {/* <h3 className='mb-4 font-medium text-xl'>Try Brewfather</h3>
 				<a
 					href='https://brewfather.app/?via=smallbatchbru'
 					target='_blank'
@@ -23,15 +41,15 @@ const SideBar = ({ batchData }) => {
 						width='150'
 						height='150'
 					/>
-				</a>
-			</section>
-			{batchData && (
-				<section>
-					<InProgressBatch batchData={batchData} />
-				</section>
-			)}
-		</div>
-	)
-}
+				</a> */}
+      </section>
+      {batchData && (
+        <section>
+          <InProgressBatch batchData={batchData} />
+        </section>
+      )}
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
